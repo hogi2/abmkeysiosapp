c0bc4f856881a2e679ea5d41c2923d6434ecdb5b
Asoft <hgnkhln1@gmail.com>
2024-07-08 02:42:20 +0300

    new updates

---
+++ abmkeys/Login/KeychainHelper.swift
@@ -0,0 +1,8 @@
+//
+//  KeychainHelper.swift
+//  abmkeys
+//
+//  Created by Hogan Alkahlan on 7/8/24.
+//
+
+import Foundation
---
+++ abmkeys/Login/QRScannerView.swift
@@ -0,0 +1,8 @@
+//
+//  QRScannerView.swift
+//  abmkeys
+//
+//  Created by Hogan Alkahlan on 7/8/24.
+//
+
+import Foundation
